4a97bf6c44a659d1c6ebc338695381ca3820073d
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-17 15:44:36 +0200

    feat: add js logic diagram

---
+++ main.js
@@ -0,0 +1,11 @@
+// mostra 5 numeri random da far memorizare
+
+// genera un array di 5 numeri random
+
+// fagli comparire sullo schermo per 30 secondi
+
+// chiedi all'utente di inserire uno alla volta i numeri che si ricorda
+
+// costrare all'utente i numeri indovinati
+
+// chiedi all'utente qual numeri gli sono stati mostrati
